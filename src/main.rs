2c14ea72e404a7844627b618346bda9f06eb3d52
Joe Lee <eojeel@users.noreply.github.com>
2024-02-15 19:43:58 +0000

    mess

--- src/main.rs
+++ src/main.rs
@@ -33,34 +33,34 @@ use commands::*;
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
 
     html! {
         <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+        <div>
+        <div class="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+        <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+            <div class="mb-4">
+                <h1 class="text-grey-darkest">{ "Todo List" }</h1>
+                <div class="flex mt-4">
+                    <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"/>
+
+                    <button class="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">{ "Add" }</button>
+                </div>
+            </div>
+            <div>
+                <div class="flex mb-4 items-center">
+                    <p class="w-full text-grey-darkest">{ "Add another component to Tailwind Components" }</p>
+                    <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">{ "Done" }</button>
+                    <button class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">{ "Remove" }</button>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
         </div>
     }
 }
 
 fn main() {
     yew::Renderer::<App>::new().render();
-}
-
-fn display_help() -> () {
-    println!("Usage: todo <command> <args>");
-
-    println!();
-    println!("Commands:");
-    println!(" Add <description> - add a new todo");
-    println!(" Done <id> - mark a todo as done");
-    println!(" List - list all todos");
-    println!();
 }
